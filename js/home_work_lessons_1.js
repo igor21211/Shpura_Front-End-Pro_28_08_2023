1328ff495f4853ef302c09fed14dd179b21cb824
igor21211 <spuraigor@gmail.com>
2023-07-31 19:45:07 +0300

    add_logic_when_promt_get_null

--- js/home_work_lessons_1.js
+++ js/home_work_lessons_1.js
@@ -1,3 +1,6 @@
 const firstName = prompt('What is your name?');
-
-alert(`Hello, ${firstName} How are you?`);
+if (firstName !== null) {
+    alert(`Hello, ${firstName}! How are you?`);
+  } else {
+    alert("Why you dont want set your name?");
+  }
