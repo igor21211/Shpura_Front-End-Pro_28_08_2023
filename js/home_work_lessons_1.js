8137e24e6631de68e753841e9f3bb2b846d5a8a1
Tobi <86198126+igor21211@users.noreply.github.com>
2023-08-02 08:17:06 +0300

    Update home_work_lessons_1.js

--- js/home_work_lessons_1.js
+++ js/home_work_lessons_1.js
@@ -1,5 +1,5 @@
 const firstName = prompt('What is your name?');
-if (firstName !== null) {
+if (firstName !== null && firstName.length > 0) {
     alert(`Hello, ${firstName}! How are you?`);
   } else {
     alert("Why you dont want set your name?");
