0eb269f3995a827ac4244ba396cfd60327167946
igor21211 <spuraigor@gmail.com>
2023-08-03 20:21:52 +0300

    add home work lessons_2

--- js/home_work_lessons_2.js
+++ js/home_work_lessons_2.js
@@ -1,9 +1,14 @@
 const fistString = prompt("What is your name?");
 const secondString = prompt("How Old are you?");
 const theeString = prompt("What language of programing do you love?")
+//first example
 if(fistString || secondString || theeString){
 alert(fistString+" "+secondString+" "+theeString);
 }
+//second example
+//if(fistString || secondString || theeString){
+//  alert(fistString.concat(secondString).concat(theeString));
+// }
 //first example
 const number = prompt("Set please number with 5 digits")
 let array = [...number.toString()].map(Number);
