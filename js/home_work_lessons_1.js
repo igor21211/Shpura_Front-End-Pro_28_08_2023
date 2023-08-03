5d53f3f839deff04840593a4d94e7fe7a7284d1b
igor21211 <spuraigor@gmail.com>
2023-08-03 20:00:18 +0300

    add home work lessons_2

--- js/home_work_lessons_1.js
+++ js/home_work_lessons_1.js
@@ -1,6 +1,2 @@
 const firstName = prompt('What is your name?');
-if (firstName !== null) {
-    alert(`Hello, ${firstName}! How are you?`);
-  } else {
-    alert("Why you dont want set your name?");
-  }
+alert(`Hello, ${firstName}! How are you?`);
---
+++ js/home_work_lessons_2.js
@@ -0,0 +1,19 @@
+const fistString = prompt("What is your name?");
+const secondString = prompt("How Old are you?");
+const theeString = prompt("What language of programing do you love?")
+if(fistString || secondString || theeString){
+alert(fistString+" "+secondString+" "+theeString);
+}
+//first example
+const number = prompt("Set please number with 5 digits")
+let array = [...number.toString()].map(Number);
+alert(array.toString())
+
+//Second example
+const number2 = prompt("Set please number with 5 digits")
+let results = number2.toString();
+for (let index = 0; index < results.length; index++) {
+    const element = results[index];
+    alert(element);
+}
+
