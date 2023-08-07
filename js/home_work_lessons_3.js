26b327402d8298d06221ca923d967cf690207507
igor21211 <spuraigor@gmail.com>
2023-08-07 20:16:39 +0300

    add home work lessons_3

---
+++ js/home_work_lessons_3.js
@@ -0,0 +1,28 @@
+let action = prompt("What do you want do the action? we have (add, sub, mult, div)")
+if(action){
+let firstNumber = prompt("please enter the first number")
+let secondNumber = prompt("please enter the second number")
+if(firstNumber && secondNumber){
+switch (action) {
+    case 'add':
+        alert(firstNumber+''+' + '+''+secondNumber+''+' = '+ firstNumber+secondNumber)
+        break;
+    case 'sub':
+        alert(firstNumber+''+' - '+''+secondNumber+''+' = '+ firstNumber-secondNumber)
+        break;
+    case 'mult':
+        alert(firstNumber+''+' * '+''+secondNumber+''+' = '+ firstNumber*secondNumber)
+        break;
+    case 'mult':
+        alert(firstNumber+''+' / '+''+secondNumber+''+' = '+ firstNumber/secondNumber)
+        break;
+    default:
+        alert('Not correct action')
+        break;
+}
+}else{
+    alert("You are cancel!!")
+}
+}else{
+    alert("You are cancel something action or number")
+}
