918a0b356b2514750a8670079679142758a05a80
igor21211 <spuraigor@gmail.com>
2023-08-31 23:43:05 +0300

    add home work lessons_1

--- js/home_work_lessons_1.js
+++ js/home_work_lessons_1.js
@@ -1,6 +1,6 @@
-const firstName = prompt('What is your name?');
+const firstName = prompt('What is your name?')
 if (firstName !== null && firstName.length > 0) {
-    alert(`Hello, ${firstName}! How are you?`);
-  } else {
-    alert("Why you dont want set your name?");
-  }
+	alert(`Hello, ${firstName}! How are you?`)
+} else {
+	alert('Why you dont want set your name?')
+}
